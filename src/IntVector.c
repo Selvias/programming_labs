784e39534a1ca56a9482d7a6339e8329fb273f26
Selvias <wells@DESKTOP-U0VVO7J.localdomain>
2021-04-01 14:47:43 +0300

    Add checks in IntVector.c file.

--- src/IntVector.c
+++ src/IntVector.c
@@ -1,132 +1,150 @@
 #include "IntVector.h"
 
 IntVector *int_vector_new(size_t init_capacity) {
-    IntVector *vect = (IntVector*)malloc(sizeof(vect));
-    printf("%ld\n", sizeof(vect));
-    vect->arr = (int*)malloc(init_capacity*sizeof(int));
-    vect->size = 0;
-    vect->capacity = init_capacity;
-    if (vect != NULL)
-        return vect;
-    else
-        return NULL;
+  IntVector *vect = (IntVector *)malloc(sizeof(IntVector));
+  if (vect == NULL)
+    return NULL;
+  vect->arr = (int *)malloc(init_capacity * sizeof(int));
+  if (vect->arr == NULL) {
+    free(vect);
+    return NULL;
+  }
+  vect->size = 0;
+  vect->capacity = init_capacity;
+
+  return vect;
 }
 
 IntVector *int_vector_copy(const IntVector *v) {
+  if (v != NULL) {
     IntVector *vect_copy = NULL;
     vect_copy = int_vector_new(v->capacity);
+    if (vect_copy == NULL)
+      return NULL;
     vect_copy->capacity = v->capacity;
     vect_copy->size = v->size;
 
-    for (int j = 0; j < v->size; j++)
-    {
-        vect_copy->arr[j] = v->arr[j];
+    for (int j = 0; j < v->size; j++) {
+      vect_copy->arr[j] = v->arr[j];
     }
-    
-    if (vect_copy != NULL)
-        return vect_copy;
-    else    
-        return NULL;
+
+    return vect_copy;
+  } else
+    return NULL;
 }
 
 int int_vector_get_item(const IntVector *v, size_t index) {
+  if ((v != NULL) && (index < v->size)) {
     int item = v->arr[index];
     return item;
+  } else
+    return -1;
 }
 
 void int_vector_set_item(IntVector *v, size_t index, int item) {
+  if (v != NULL && index < v->size)
     v->arr[index] = item;
-    v->size += 1;
 }
 
 size_t int_vector_get_size(const IntVector *v) {
+  if (v != NULL)
     return v->size;
 }
 
-size_t int_vector_get_capacity(const IntVector *v) {
-    return v->capacity;
+size_t int_vector_get_capacity(const IntVector *v) { 
+  if (v != NULL)
+    return v->capacity; 
 }
 
-int int_vector_push_back(IntVector *v, int item) { 
+int int_vector_push_back(IntVector *v, int item) {
 
-    if (v->capacity == 0) {
-        printf("ADDITION FAILED\n");
-        exit(-1);
-    } else if (v->size < v->capacity) {
-        v->arr[v->size] = item;
-        ++(v->size);
-        return 0;
-    } else if (v->size >= v->capacity) {                        
-        int_vector_reserve(v, 2*(v->capacity));
-        int_vector_push_back(v, item);
-    }
+  short int flag = 0;
 
+  if (v->capacity == 0) {
+    printf("ADDITION FAILED\n");
+    return -1;
+  } else if (v->size < v->capacity) {
+    v->arr[v->size] = item;
+    ++(v->size);
     return 0;
+  } else if (v->size >= v->capacity) {
+    flag = int_vector_reserve(v, 2 * (v->capacity));
+    if (flag == -1)
+      return -1;
+    int_vector_push_back(v, item);
+    return 0;
+  } else
+    return -1;
 }
 
 void int_vector_pop_back(IntVector *v) {
-    if (v->size != 0) 
-    {
-        v->arr[v->size - 1] = 0;
-        (v->size)--;
-    }
-
+  if ((v != NULL) && (v->size != 0) && (v->capacity != 0)) {
+    v->arr[v->size - 1] = 0;
+    (v->size)--;
+  }
 }
 
 int int_vector_shrink_to_fit(IntVector *v) {
-    if (v != NULL)
-    {
-        v->arr = (int*)realloc(v->arr, v->size*sizeof(int));
-        v->capacity = v->size;
-        return 0; 
-    }
-
+  if (v != NULL && v->capacity != 0 && v->size != 0) {
+    v->arr = (int *)realloc(v->arr, v->size * sizeof(int));
     if (v->arr == NULL)
-        return -1;
-
+      return -1;
+    v->capacity = v->size;
     return 0;
+  } else
+    return -1;
 }
 
 int int_vector_resize(IntVector *v, size_t new_size) {
 
-    if (new_size > v->size && new_size > v->capacity) {
-        size_t temp_size = v->size;
-        int_vector_reserve(v, 2*(new_size));
-        for (int i = temp_size; i < new_size; i++)
-        {
-            v->arr[i] = 0;
-        }
-        v->size = new_size;
-        return 0;
-    } else if (new_size < v->size) {
-        v->size = new_size;
-        int_vector_shrink_to_fit(v);
-        return 0;
-    } else if (new_size > v->size && new_size < v->capacity) {
-        size_t temp_size = v->size;
-        int_vector_reserve(v, 2*(new_size));
-        for (int i = temp_size; i < new_size; i++)
-        {
-            v->arr[i] = 0;
-        }
-        v->size = new_size;
-        return 0;
-    } else
-        return -1;
+  short int flag = 0;
+
+  if (v == NULL)
+    return -1;
+
+  if (new_size > v->size && new_size > v->capacity) {
+    size_t temp_size = v->size;
+    flag = int_vector_reserve(v, 2 * (new_size));
+    if (flag == -1)
+      return flag;
+    for (int i = temp_size; i < new_size; i++) {
+      v->arr[i] = 0;
+    }
+    v->size = new_size;
+    return 0;
+  } else if (new_size < v->size && new_size != 0) {
+    v->size = new_size;
+    flag = int_vector_shrink_to_fit(v);
+    if (flag == -1)
+      return flag;
+    return 0;
+  } else if (new_size < v->size && new_size == 0) {
+    v->size = new_size;
+    return 0;
+  } else if (new_size > v->size && new_size <= v->capacity) {
+    size_t temp_size = v->size;
+    for (int i = temp_size; i < new_size; i++) {
+      v->arr[i] = 0;
+    }
+    v->size = new_size;
+    return 0;
+  } else
+    return -1;
 }
 
 int int_vector_reserve(IntVector *v, size_t new_capacity) {
-    
-    if (new_capacity > v->capacity)
-    {
-        v->arr = (int*) realloc(v->arr, new_capacity*sizeof(int));
-        v->capacity = new_capacity;
-        return 0;
-    } else 
-        return -1;
+
+  if (v != NULL && new_capacity > v->capacity && new_capacity > 0) {
+    v->arr = (int *)realloc(v->arr, new_capacity * sizeof(int));
+    if (v->arr == NULL)
+      return -1;
+    v->capacity = new_capacity;
+    return 0;
+  } else
+    return -1;
 }
 
 void int_vector_free(IntVector *v) {
-    free(v->arr);
-    free(v);
+  free(v->arr);
+  free(v);
 }
--- src/main.c
+++ src/main.c
@@ -2,59 +2,88 @@
 
 int main() {
 
-    static int g_item, s_item = 25, g_size, g_capacity;
-    static size_t  init_capacity = 10;
+  static int g_item, s_item = 25, g_size, g_capacity;
+  static size_t init_capacity = 10;
 
-    IntVector *v = NULL;
-    IntVector *v_copy = NULL;
+  IntVector *v = NULL;
+  IntVector *v_copy = NULL;
 
-    //1. Создание нового вектора.
-    v = int_vector_new(init_capacity);
+  // 1. Создание нового вектора.
+  v = int_vector_new(init_capacity);
 
-    //2.Добавление элементов в конец массива.
-    for (int i = 0; i < 11; i++)
-    {
-        int_vector_push_back(v, i);
+  // 2.Добавление элементов в конец массива.
+  if (v != NULL) {
+    for (int i = 0; i < 11; i++) {
+      int_vector_push_back(v, i);
     }
+  }
 
-    //3.Копия вектора
+  // 3.Копия вектора
+  if (v != NULL) {
     v_copy = int_vector_copy(v);
-
-    //4.Получение элемента по индексу
+  }
+  // 4.Получение элемента по индексу
+  if (v != NULL) {
     g_item = int_vector_get_item(v, 10);
+  }
 
-    //5.Установление значения по индексу
-    int_vector_set_item(v, 5,s_item);
+  // 5.Установление значения по индексу
+  if (v != NULL) {
+    int_vector_set_item(v, 5, s_item);
+  }
 
-    //6.Получение размера
+  // 6.Получение размера
+  if (v != NULL) {
     g_size = int_vector_get_size(v);
-
-    //7.Получение ёмкости
+  }
+  // 7.Получение ёмкости
+  if (v != NULL) {
     g_capacity = int_vector_get_capacity(v);
+  }
 
-    printf("G_ITEM : %d\nCHECK_SET_ITEM : %d\nG_SIZE : %d\nG_CAPACITY : %d\n", g_item, v->arr[5], g_size, g_capacity);
+  printf("G_ITEM : %d\nCHECK_SET_ITEM : %d\nG_SIZE : %d\nG_CAPACITY : %d\n",
+         g_item, v->arr[5], g_size, g_capacity);
 
-    printf("VECTOR_FILLING : ");
-
-    for (int i = 0; i < 11; i++)
-    {
-        printf("%d",v->arr[i]);
-        printf(" ");
+  printf("VECTOR_FILLING : ");
+  if (v != NULL) {
+    for (int i = 0; i < 11; i++) {
+      printf("%d", v->arr[i]);
+      printf(" ");
     }
+  }
 
-    //8. 
+  // 8. Добавление элемента в конец массива
+  if (v != NULL) {
     int_vector_push_back(v, 24);
-    
-    //9.
+  }
+  // 9. Изменение размера массива
+  if (v != NULL) {
     int_vector_resize(v, 9);
-
-    //10.
+  }
+  // 10. Удаление последнего элемента массива
+  if (v != NULL) {
     int_vector_pop_back(v);
+  }
 
-    printf("\nSIZE_VECTOR : %d\nCAPACITY_VECTOR : %d\n", v->size, v->capacity);
+  // 10.1. Изменение размера массива (граничный случай)
+  if (v != NULL) {
+    int_vector_resize(v, 0);
+  }
 
-    //11.
+    printf("CAP_TEST : %d\n", v->capacity);
+
+  // 10.2. Добавление элемента в конец массива
+  if (v != NULL) {
+    int_vector_push_back(v, 25);
+  }
+
+  printf("\nSIZE_VECTOR : %d\nCAPACITY_VECTOR : %d\n", v->size, v->capacity);
+
+  // 11. Освобождение памяти из под вектора
+  if (v != NULL && v_copy != NULL) {
     int_vector_free(v);
+    int_vector_free(v_copy);
+  }
 
-    return 0;
+  return 0;
 }
