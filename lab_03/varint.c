80ce63d2fc9eba4595aea94cb82b5d984851b538
Vladimir <novel@MSI.localdomain>
2021-04-09 14:46:36 +0700

    Rewrite code with fwrite() & fread() functions.

--- lab_03/varint.c
+++ lab_03/varint.c
@@ -10,9 +10,10 @@ uint32_t generate_number();
 
 int main () {
 
+    int array[100] = {0};
+
     uint8_t* buffer = NULL;                                             //Выделить память
-    // buffer = (uint8_t*)malloc(4*sizeof(uint8_t)); 
-    buffer = (uint8_t*)malloc(sizeof(uint8_t)); 
+    buffer = (uint8_t*)malloc(4*sizeof(uint8_t)); 
     FILE *out_uncompressed;    
     FILE *out_compressed;
     FILE *out_decompressed;
@@ -22,13 +23,14 @@ int main () {
 
     uint32_t digit = 0;
 
-    for (int i = 0; i < 1000000; i++) {
-        fprintf(out_uncompressed, "%d\n", digit = generate_number());   //Вызов генерации числа 
-                                                                        //Кодировка работает некорректно с числами, использующими более чем один байт для своего представления
-        encode_varint(digit, buffer);
+    for (int i = 0; i < 100; i++) {
 
-        fprintf(out_compressed, "%d\n", *buffer);                       //Запись в файл с учётом использованного количества байт (Возвращаемое кодировщиком значение)
+        fprintf(out_uncompressed, "%d\n", digit = generate_number());   //Вызов генерации числа    
 
+        int sz = encode_varint(digit, buffer);  
+        array[i] = sz;
+
+        fwrite(buffer, 1, sz, out_compressed);
     }
 
     fclose(out_uncompressed);
@@ -37,51 +39,55 @@ int main () {
     out_compressed = fopen("compressed.dat", "r");
     out_decompressed = fopen("decompressed.dat", "w");
 
-    // const uint8_t **val = (const uint8_t**)malloc(sizeof(uint8_t*));
 
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < 100; i++) {
+        uint8_t **val = (uint8_t**)malloc(sizeof(uint8_t*));
+        *val = (uint8_t*)calloc(4, sizeof(uint8_t));
 
-        const uint8_t *val = (uint8_t*)malloc(4*sizeof(uint8_t));
-        fscanf(out_compressed, "%d", val);
-        // printf("%d", val[0]);
-        uint32_t uncode = decode_varint(&val);
+        fread(*val, 1, array[i], out_compressed);
+
+        const uint8_t **val_cp = (const uint8_t**)val;
+
+        uint32_t uncode = decode_varint(val_cp);
         fprintf(out_decompressed, "%u\n", uncode);
     }
 
     fclose(out_compressed);
     fclose(out_decompressed);
 
+    free(buffer);
     return 0;
 }
 
 size_t encode_varint(uint32_t value, uint8_t* buf)
 {
     assert(buf != NULL);
-    uint8_t* cur = buf;                                               //    |1....2....3....4....|....
-    while (value >= 0x80) {                                          //
-        const uint8_t byte = (value & 0x7f) | 0x80;                 //      11010101 10101010  Формирование порядка Little Endian             
-        *cur = byte;                                               //
-        value >>= 7;                                              //        00000000 00000011
-        ++cur;                                                   //                  10101011
-    }                                                           //                   10000000
-    *cur = value;                                              //                    10101010 10101011 00000011
-    ++cur;                                                    //
-    buf = (uint8_t*)realloc(buf, (cur - buf)*(sizeof(uint8_t)));
-    return cur - buf;                                        //             return Количество байт под закодированное число
+    uint8_t* cur = buf; 
+                                  
+    while (value >= 0x80) {                      
+        const uint8_t byte = (value & 0x7f) | 0x80;                         
+        *cur = byte;                                              
+        value >>= 7;                                             
+        ++cur;                                                   
+    }             
+    *cur = value;                                
+    ++cur;                                               
+    return cur - buf;                                        
 }
 
 uint32_t decode_varint(const uint8_t** bufp)
 {
-    const uint8_t* cur = *bufp;
-    uint8_t byte = *cur++;
-    uint32_t value = byte & 0x7f;
-    size_t shift = 7;
-    while (byte >= 0x80) {
+    const uint8_t* cur = *bufp;                             //Указатель на область в памяти, где хранится считанное скомпрессированное число (первый байт)
+    uint8_t byte = *cur++;                                  //Переменной присваивается адрес текущего считываемого байта, затем указатель на область в памяти сдвигается на 1
+    uint32_t value = byte & 0x7f;                           //Побитовое и числа и 01111111
+    size_t shift = 7;                                       //Переменная сдвига на 7 бит (для того, чтобы вернуть исходный порядок байт)
+    while (byte >= 0x80) {                                  //Пока число (байт) больше либо равно 128 (если число занимает 1 байт, то цикл не выполняется
         byte = *cur++;
-        value += (byte & 0x7f) << shift;
+        value += ((uint32_t)(byte & 0x7f) << shift);
         shift += 7;
     }
     *bufp = cur;
+    // printf("VAL : %d\n", value);
     return value;
 }
 
